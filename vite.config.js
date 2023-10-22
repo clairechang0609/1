e3f261bea2042d588aa2a7393039d30293952b22
Claire.Chang <claire.chang@starclinic.com.tw>
2023-10-22 21:56:02 +0800

    add meta tags

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
+import Pages from 'vite-plugin-pages'
+import generateSitemap from 'vite-plugin-pages-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig((mode) => {
 	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
 	return {
-		plugins: [vue()],
+		plugins: [
+			vue(),
+			Pages({
+				onRoutesGenerated: routes => (generateSitemap({
+					routes,
+					readable: true
+				}))
+			})
+		],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src')
