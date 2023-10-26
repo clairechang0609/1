81d04de084a41720bf3a2e4ebe0f872674f79c7d
Claire.Chang <claire.chang@starclinic.com.tw>
2023-10-26 22:22:43 +0800

    fix: router error

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,13 @@
 import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
-import Pages from 'vite-plugin-pages'
-import generateSitemap from 'vite-plugin-pages-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig((mode) => {
 	process.env = {...process.env, ...loadEnv(mode, process.cwd())};
 
 	return {
-		plugins: [
-			vue(),
-			Pages({
-				onRoutesGenerated: routes => (generateSitemap({
-					routes,
-					readable: true
-				}))
-			})
-		],
+		plugins: [vue()],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, './src')
